ffbf9189ff775457e293d2ecaed5e4134c83ac6e
iitgituser <gituseriit@innovationit.com.bd>
2022-03-02 11:41:34 +0600

    binary serch result

--- binary_search.php
+++ binary_search.php
@@ -2,20 +2,28 @@
 
 $A = [10,12,13,14,15,16,17,18,19,20,23,25,26,27,28,29,30,32,35,39,40,41,45,46,47,49,50];
 $n = count($A);
-$x = 13;
-echo bineary_search($A,$n,$x);
+// echo $n;
+$x = 9;
+// echo $A[bineary_search((array)$A,(int)$n,(int)$x)];
+// echo bineary_search((array)$A,(int)$n,(int)$x);
+$result = bineary_search((array)$A,(int)$n,(int)$x);
+if($result < 0){
+    echo "The Search result '{$x}' not found in this List";
+}else{
+    echo $A[$result];
+}
 
-function bineary_search($A,$n,$x){
+function bineary_search($A,$n,$x):int{
     $left = 0;
     $right = $n - 1;
     $mid = 0;
 
     while ($left <= $right) {
         
-        $mid = ($left + $right) / 2;
+        $mid = (int)($left + $right) / 2;
         
         if($A[$mid] == $x){
-            return (int)$mid;
+            return $mid;
         }
 
         if($A[$mid] < $x){
